c44ca659a0e2634c51bb76fb4db364ec77058bff
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2016-06-10 16:22:56 -0300

    Add "institutionAcronym" column to salas-instituicoes.rb

--- portal/salas-instituicoes.rb
+++ portal/salas-instituicoes.rb
@@ -1,8 +1,14 @@
-print "meetingID\tname\tinstitution\n"
+print "meetingID\tname\tinstitution\tinstitutionAcronym\n"
 BigbluebuttonRoom.all.order(:name).each do |r|
   if r.owner && !r.owner.disabled?
     print "#{r.meetingid}\t#{r.name}\t"
-    print r.owner.institution.name if r.owner.institution
+    if r.owner.institution
+      print r.owner.institution.name
+      print "\t"
+      print r.owner.institution.acronym
+    else
+      print "\t"
+    end
     print "\n"
   end
 end
